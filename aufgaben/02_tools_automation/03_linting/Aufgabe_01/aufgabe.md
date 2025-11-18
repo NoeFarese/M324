017d0090e24bd7675107f9d3a344fa79359c3177
Noé <110814367+NoeFarese@users.noreply.github.com>
2025-11-18 08:05:06 +0100

    start with 03 Linting - Aufgabe 1

---
+++ aufgaben/02_tools_automation/03_linting/Aufgabe_01/aufgabe.md
@@ -0,0 +1,2 @@
+# 1. Wie hoch ist die Test Coverage?
+44.4 %
---
+++ aufgaben/02_tools_automation/03_linting/Aufgabe_01/src/house.py
@@ -0,0 +1,16 @@
+class House:
+    def __init__(self):
+        print("New house was builded")
+
+    def get_name(self):
+        print(self.name)
+
+    def set_name(self, name):
+        if type(name) is not str:
+            raise Exception("name must be a string")
+
+        self.name = name
+
+    @staticmethod
+    def get_price(self):
+        print("50" + " CHF")
